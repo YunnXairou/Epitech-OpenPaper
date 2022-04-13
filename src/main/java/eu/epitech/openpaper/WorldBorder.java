9281f30f0cd61f7828e386e81c4674e8a1e53f2b
Clément Beaubestre <clement.beaubestre@epitech.eu>
2022-04-13 22:05:30 +0200

    Add border change with bosses
    only one time by player

--- src/main/java/eu/epitech/openpaper/WorldBorder.java
+++ src/main/java/eu/epitech/openpaper/WorldBorder.java
@@ -5,15 +5,21 @@ import java.util.Iterator;
 
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.Statistic;
 import org.bukkit.World;
 import org.bukkit.advancement.Advancement;
+import org.bukkit.entity.EntityType;
 
 public class WorldBorder {
     static public ArrayList<Advancement> trophies;
+    static public ArrayList<OfflinePlayer> witherKiller;
+    static public ArrayList<OfflinePlayer> dragonKiller;
 
     static public void compute() {
         double expantion = OpenPaper.plugin.getConfig().getDouble("worldborder.base")
-                + trophies.size() * OpenPaper.plugin.getConfig().getDouble("worldborder.onAchievment");
+                + trophies.size() * OpenPaper.plugin.getConfig().getDouble("worldborder.onAchievment")
+                + witherKiller.size() * OpenPaper.plugin.getConfig().getDouble("worldborder.witherKiller")
+                + dragonKiller.size() * OpenPaper.plugin.getConfig().getDouble("worldborder.dragonKiller");
 
         for (World w : Bukkit.getWorlds())
             w.getWorldBorder().setSize(expantion);
@@ -21,6 +27,7 @@ public class WorldBorder {
 
     WorldBorder() {
         getTrophies();
+        getStats();
 
         compute();
     }
@@ -41,5 +48,18 @@ public class WorldBorder {
                     break;
                 }
         }
+    }
+
+    private void getStats() {
+        witherKiller = new ArrayList<>();
+        dragonKiller = new ArrayList<>();
+
+        for (OfflinePlayer p : Bukkit.getOfflinePlayers()) {
+            if (p.getStatistic(Statistic.KILL_ENTITY, EntityType.WITHER) > 0)
+                witherKiller.add(p);
+            if (p.getStatistic(Statistic.KILL_ENTITY, EntityType.ENDER_DRAGON) > 0)
+                dragonKiller.add(p);
+        }
+
     }
 }
